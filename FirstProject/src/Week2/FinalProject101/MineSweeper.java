6054832f4ff5105b631510048c82419f34ec502c
Türker <kivturker@gmail.com>
2023-06-15 10:49:13 +0300

    finish mineSweeper

--- FirstProject/src/Week2/FinalProject101/MineSweeper.java
+++ FirstProject/src/Week2/FinalProject101/MineSweeper.java
@@ -1,5 +1,7 @@
 package Week2.FinalProject101;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -9,9 +11,17 @@ public class MineSweeper {
 
         System.out.print("Enter row count: ");
         int matrixRowCount = inp.nextInt();
+        if(matrixRowCount <= 0){
+            System.out.println("Please enter a valid size!");
+            System.exit(0);
+        }
 
         System.out.print("Enter column count: ");
         int matrixColumnCount = inp.nextInt();
+        if(matrixColumnCount <= 0){
+            System.out.println("Please enter a valid size!");
+            System.exit(0);
+        }
 
         String[][] matrix = new String[matrixRowCount][matrixColumnCount];
         for(int i = 0; i < matrixRowCount; i++)
@@ -25,6 +35,9 @@ public class MineSweeper {
         //placing random mines into the field
         int totalElementCount = matrixColumnCount * matrixRowCount;
         int mineCount = totalElementCount / 4;
+        List<String> minePositions = new ArrayList<>();
+        List<String> selectedPositions = new ArrayList<>();
+
         for(int i = 0; i < mineCount; i++)
         {
             int randomRow = (int)(Math.random() * (matrixRowCount - 1));
@@ -36,7 +49,9 @@ public class MineSweeper {
                 randomColumn = (int)(Math.random() * (matrixColumnCount - 1));
             }
 
-            matrix[randomRow][randomColumn] = " * ";
+            String pos = randomRow + "-" + randomColumn;
+            minePositions.add(pos);
+            //matrix[randomRow][randomColumn] = " * ";
         }
 
         printMatrix(matrix);
@@ -46,25 +61,35 @@ public class MineSweeper {
 
         //Playing
         while(true) {
+
             System.out.print("Enter the row of the point you wanna select: ");
             int selectedRow = inp.nextInt();
+            if(selectedRow >= matrixRowCount){
+                System.out.println("Enter valid point!");
+                continue;
+            }
 
             System.out.print("Enter the column of the point you wanna select: ");
             int selectedColumn = inp.nextInt();
+            if(selectedColumn >= matrixColumnCount){
+                System.out.println("Enter valid point!");
+                continue;
+            }
 
-            if(matrix[selectedRow][selectedColumn] == " * ")
+            String selectedPos = selectedRow + "-" + selectedColumn;
+            if(minePositions.contains(selectedPos))
             {
                 System.out.print("You pressed mine. GAME OVER!!!");
                 break;
             }
             else {
-                currentScore++;
-                if(minelessCount == currentScore) {
-                    System.out.print("You won the game!!!!!!!!");
-                    break;
+                if(selectedPositions.contains(selectedPos)) {
+                    System.out.println("You already selected this point!!!");
+                    continue;
                 }
-
-                System.out.print("Nice. Keep going!");
+                selectedPositions.add(selectedPos);
+                currentScore++;
+                System.out.println("Nice. Keep going!");
 
                 //searching mines around the selected point
                 int rowStart = selectedRow - 1;
@@ -73,20 +98,26 @@ public class MineSweeper {
                 int columnEnd = selectedColumn + 1;
 
                 int mineCountAround = 0;
-
                 for(int i = rowStart; i <= rowEnd; i++)
                 {
                    for(int j = columnStart; j <= columnEnd; j++)
                    {
-                        if(matrix[i][j] == " * ")
+                       if(i < 0 || j < 0 || i >= matrix.length || j >= matrix[i].length) continue;
+                       String currentPos = i + "-" + j;
+                       if(minePositions.contains(currentPos))
                         {
                             mineCountAround++;
                         }
                    }
                 }
 
-                matrix[selectedRow][selectedColumn] = String.valueOf(mineCountAround);
+                matrix[selectedRow][selectedColumn] = " " + mineCountAround + " ";
                 printMatrix(matrix);
+
+                if(minelessCount == currentScore) {
+                    System.out.print("You won the game!!!!!!!!");
+                    break;
+                }
             }
         }
     }
