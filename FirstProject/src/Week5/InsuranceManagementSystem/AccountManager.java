e59baac2ced46726d363523217d4b40d49215348
turkerkiv <99351028+turkerkiv@users.noreply.github.com>
2023-08-03 12:08:26 +0300

    add new comparator to accounts treeset

--- FirstProject/src/Week5/InsuranceManagementSystem/AccountManager.java
+++ FirstProject/src/Week5/InsuranceManagementSystem/AccountManager.java
@@ -1,15 +1,27 @@
 package Week5.InsuranceManagementSystem;
 
+import java.util.Comparator;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class AccountManager {
-    private final Set<Account> accounts = new TreeSet<>();
-    //todo add comparator
+    private final Set<Account> accounts = new TreeSet<>(new Comparator<Account>() {
+        @Override
+        public int compare(Account o1, Account o2) {
+            return o1.compareTo(o2);
+        }
+    });
 
-    public void logIn(String email, String password)
+    public Account logIn(String email, String password)
     {
-        //todo
+        for(Account a : accounts)
+        {
+            //todo instead of boolean use try catch and custom invalidauthenticationexception
+            if(a.logIn(email,password)){
+                return a;
+            }
+        }
+        return null;
     }
 
     public void addAccount(Account acc)
