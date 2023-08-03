40e9dc970e5eedd6cc5f7f85e63fa7f3294b5420
turkerkiv <99351028+turkerkiv@users.noreply.github.com>
2023-08-03 12:06:33 +0300

    implement comparable & login methods

--- FirstProject/src/Week5/InsuranceManagementSystem/Account.java
+++ FirstProject/src/Week5/InsuranceManagementSystem/Account.java
@@ -3,29 +3,60 @@ package Week5.InsuranceManagementSystem;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class Account{
+public abstract class Account implements Comparable{
     private AuthenticationStatus logInStatus;
-    private  User user;
+    private final User user;
     private List<Insurance> activeInsurances = new ArrayList<>();
+
+    Account(User user)
+    {
+        this.user = user;
+    }
+
+    public final void showUserInfo()
+    {
+        System.out.println("Welcome " + user.getFirstName() + " " + user.getLastName());
+        System.out.println("Here is your information: ");
+        System.out.println("Age: " + user.getAge());
+        System.out.println("Email: " + user.getEmail());
+        System.out.println("Job: " + user.getJob());
+
+        for(int i = 0; i < user.getAddresses().size(); i++)
+        {
+            System.out.println("Address " + i + ": " + user.getAddresses().get(i));
+        }
+    }
+
+    public boolean logIn(String email, String password)
+    {
+        if(user.getEmail().equals(email) && user.getPassword().equals(password))
+        {
+            logInStatus = AuthenticationStatus.SUCCESS;
+            return true;
+        }
+        else {
+            logInStatus = AuthenticationStatus.FAIL;
+            return false;
+        }
+    }
 
     public AuthenticationStatus getLogInStatus() {
         return logInStatus;
     }
 
-    public final void showUserInfo()
+    public int hashCode()
     {
-        //todo
+        return user.getEmail().hashCode() + user.getFirstName().hashCode();
     }
 
-    public void logIn(String email, String password)
-    {
-        //todo
+    public boolean equals(Object obj) {
+        return obj.hashCode() == hashCode();
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return Integer.compare(hashCode(), ((Account)o).hashCode());
     }
 
     public abstract void addInsurancePolicy();
-
-    //todo implement comparable
-    //todo hashcode
-    //todo equals
-    //todo compareto
 }
